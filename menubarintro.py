a2809f03f28f880b9d90421aab1eee60d358f977
erina-ameen <erina.ameen@gmail.com>
2025-07-01 14:40:02 +0100

    menubarintro.py

--- menubarintro.py
+++ menubarintro.py
@@ -27,6 +27,49 @@ edit.add_command(label="Cut", command=None)
 edit.add_command(label="Copy", command=None)
 edit.add_command(label="Paste", command=None)
 
+#Adding selection menu
+select=Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label="Selection", menu=select)
+select.add_command(label="Select All", command=None)
+select.add_command(label="Expand Selection", command=None)
+select.add_command(label="Shrink Selection", command=None)
+select.add_separator()
+select.add_command(label="Copy Line Up", command=None)
+select.add_command(label="Copy Line Down", command=None)
+select.add_command(label="Move Line Up", command=None)
+select.add_command(label="Move Line Down", command=None)
+select.add_command(label="Duplicate Selection", command=None)
+
+#Adding view menu
+view=Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label="View", menu=view)
+view.add_command(label="Command Palette", command=None)
+view.add_command(label="Open View", command=None)
+view.add_separator()
+view.add_command(label="Zoom In", command=None)
+view.add_command(label="Zoom Out", command=None)
+
+#Adding go menu
+go=Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label="Go", menu=go)
+go.add_command(label="Back", command=None)
+go.add_command(label="Forward", command=None)
+go.add_command(label="Last Edit Selection", command=None)
+go.add_separator()
+go.add_command(label="Switch Editor", command=None)
+go.add_command(label="Switch Group", command=None)
+
+#Adding run menu
+run=Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label="Run", menu=view)
+run.add_command(label="Start Debugguing", command=None)
+run.add_command(label="Run without Debugging", command=None)
+run.add_command(label="Stop without Debugging", command=None)
+run.add_command(label="Restart Debugging", command=None)
+run.add_separator()
+run.add_command(label="Open Configurations", command=None)
+run.add_command(label="Add Configuration", command=None)
+
 #Displaying Menu
 screen.config(menu=menu_bar)
 
