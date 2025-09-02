7fea2b326a30cc46ede2e207b5ba5c00182f19b2
erina-ameen <erina.ameen@gmail.com>
2025-09-02 15:58:33 +0100

    Add files via upload

---
+++ 17filedialog.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter.filedialog import *
+
+screen=Tk()
+screen.geometry("500x500")
+
+save=Button(screen, text="Save")
+add=Button(screen, text="Add")
+delete=Button(screen, text="Delete")
+open=Button(screen, text="Open")
+entry=Entry(screen)
